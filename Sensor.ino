4f004ac6a81d87c955ccab742ab9e8eaab48fa2a
Ethancao123 <ethan.cao123456@gmail.com>
2022-02-18 19:20:57 -0800

    pogworks

--- Sensor.ino
+++ Sensor.ino
@@ -1,17 +1,22 @@
-#include <SoftWire.h>
+#include <SoftwareWire.h>
 
 #include <Wire.h>
 
  
 #define I2C_ADDR 0x52
-#define SDA2 3
-#define SCL2 5
-#define OUTPUT A3
+#define SDA2 5
+#define SCL2 6
+#define OUTPUT 10
  
 uint8_t readBuff[9];
 uint8_t readBuff2[9];
 
-SoftWire i2c(SDA2, SCL2);
+uint8_t sensor1min = 5;
+uint8_t sensor2min = 5;
+bool ball1 = false;
+bool ball2 = false;
+
+SoftwareWire i2c(SDA2, SCL2);
 
 void setup() {
   Wire.begin();
@@ -27,12 +32,33 @@ void setup() {
 void loop() {
   i2cRead(0x08,readBuff,2);
   i2cRead2(0x08,readBuff2,2);
-  Serial.print(readBuff[0] + readBuff[1]);
+  int dist1 = readBuff[0] + readBuff[1];
+  int dist2 = readBuff2[0] + readBuff2[1];
+  if(dist1 < sensor1min)
+    sensor1min = dist1;
+  if(dist2 < sensor2min)
+    sensor2min = dist2;
+  if(dist1 > sensor1min + 1)
+    ball1 = true;
+  else
+    ball1 = false;
+  if(dist2 > sensor2min + 1)
+    ball2 = true;
+  else
+    ball2 = false;
+  Serial.print(ball1);
   Serial.print("  ");
-  Serial.print(readBuff2[0] + readBuff2[1]);
+  Serial.print(ball2);
   Serial.println(" ");
-  delay(25);
   //Serial.println("loop");
+  if(ball1 && ball2)
+    analogWrite(OUTPUT, 255);
+  else if(ball2)
+    analogWrite(OUTPUT, 204);
+  else if(ball1)
+    analogWrite(OUTPUT, 153);
+  else
+    analogWrite(OUTPUT, 102);
 }
  
  
@@ -67,7 +93,7 @@ void i2cRead2(uint8_t reg,uint8_t *val,uint16_t len){
     i2c.endTransmission();
     i2c.requestFrom(I2C_ADDR, len);
     int i = 0;
-    while(Wire.available()){
-      val[i++] = Wire.read();
+    while(i2c.available()){
+      val[i++] = i2c.read();
     }
 }
