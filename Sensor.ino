94273c987110c9cbbb94ea971ef0e500fb486bcd
Ethancao123 <ethan.cao123456@gmail.com>
2022-02-18 17:44:17 -0800

    Update Sensor.ino

--- Sensor.ino
+++ Sensor.ino
@@ -1,48 +1,37 @@
+#include <SoftWire.h>
+
 #include <Wire.h>
- 
-//These values are the readings of the whitepoint of the led. Take the back of vinyl sticker and put it againts face of sensor
-#define LED_RED 7540.0f
-#define LED_GREEN 14470.0f
-#define LED_BLUE 7270.0f
- 
-//Calculate the balancing factors
-#define BAL_RED (LED_GREEN/LED_RED)
-#define BAL_GREEN (LED_GREEN/LED_GREEN) 
-#define BAL_BLUE (LED_GREEN/LED_BLUE)
+
  
 #define I2C_ADDR 0x52
+#define SDA2 3
+#define SCL2 5
+#define OUTPUT A3
  
 uint8_t readBuff[9];
-uint16_t red=0;
-uint16_t blue=0;
- 
+uint8_t readBuff2[9];
+
+SoftWire i2c(SDA2, SCL2);
+
 void setup() {
   Wire.begin();
-  Serial.begin(4800);
+  i2c.begin();
+  Serial.begin(9600);
   i2cWrite(0x00,0b0111);  //enable light sensor and activate rgb mode and enable proximity sensor
   i2cWrite(0x04,0b01000000); //set to 16 bit resolution for 25ms response time and set measurement rate to 25ms
+  i2cWrite2(0x00,0b0111);  //enable light sensor and activate rgb mode and enable proximity sensor
+  i2cWrite2(0x04,0b01000000); //set to 16 bit resolution for 25ms response time and set measurement rate to 25ms
  // i2cWrite(0x01,0b00110111); //set ir led to 60khz and 125mA
 }
  
 void loop() {
-  i2cRead(0x0A,readBuff,12);
-
-  blue=(readBuff[7]<<8)|readBuff[6];
-  red=(readBuff[10]<<8)|readBuff[9];
- 
-  red*=BAL_RED;
-  blue*=BAL_BLUE;
-  
-  delay(100);
-
   i2cRead(0x08,readBuff,2);
+  i2cRead2(0x08,readBuff2,2);
   Serial.print(readBuff[0] + readBuff[1]);
-  Serial.print(" ");
-  Serial.print(red);
-  Serial.print(" ");
-  Serial.print(blue);
+  Serial.print("  ");
+  Serial.print(readBuff2[0] + readBuff2[1]);
   Serial.println(" ");
-  delay(100);
+  delay(25);
   //Serial.println("loop");
 }
  
@@ -53,11 +42,30 @@ void i2cWrite(uint8_t reg, uint8_t val){
     Wire.write(val);
     Wire.endTransmission();
 }
+
+void i2cWrite2(uint8_t reg, uint8_t val){
+    i2c.beginTransmission(I2C_ADDR);
+    i2c.write(reg);
+    i2c.write(val);
+    i2c.endTransmission();
+}
+
 void i2cRead(uint8_t reg,uint8_t *val,uint16_t len){
     Wire.beginTransmission(I2C_ADDR);
     Wire.write(reg);
     Wire.endTransmission();
     Wire.requestFrom(I2C_ADDR, len);
+    int i = 0;
+    while(Wire.available()){
+      val[i++] = Wire.read();
+    }
+}
+
+void i2cRead2(uint8_t reg,uint8_t *val,uint16_t len){
+    i2c.beginTransmission(I2C_ADDR);
+    i2c.write(reg);
+    i2c.endTransmission();
+    i2c.requestFrom(I2C_ADDR, len);
     int i = 0;
     while(Wire.available()){
       val[i++] = Wire.read();
